bead7f2ee4b8cf8105ee2c204ff644fa3308fdcb
Harshshah6 <harshsandeep23@gmail.com>
2024-12-27 15:20:22 +0530

    modified:   app/release/app-release.apk 	modified:   app/release/baselineProfiles/0/app-release.dm 	modified:   app/release/baselineProfiles/1/app-release.dm 	modified:   app/src/main/java/com/harsh/shah/saavnmp3/activities/SettingsActivity.java 	modified:   app/src/main/java/com/harsh/shah/saavnmp3/utils/customview/MaterialCustomSwitch.java 	new file:   app/src/main/res/drawable/round_menu_24.xml 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/layout/activity_settings.xml

--- app/src/main/java/com/harsh/shah/saavnmp3/activities/SettingsActivity.java
+++ app/src/main/java/com/harsh/shah/saavnmp3/activities/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.harsh.shah.saavnmp3.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,10 +18,55 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        final SettingsSharedPrefManager settingsSharedPrefManager = new SettingsSharedPrefManager(this);
 
+        binding.downloadOverCellular.setOnCheckChangeListener(settingsSharedPrefManager::setDownloadOverCellular);
+        binding.highQualityTrack.setOnCheckChangeListener(settingsSharedPrefManager::setHighQualityTrack);
+        binding.storeInCache.setOnCheckChangeListener(settingsSharedPrefManager::setStoreInCache);
+        binding.explicit.setOnCheckChangeListener(settingsSharedPrefManager::setExplicit);
+
+        binding.downloadOverCellular.setChecked(settingsSharedPrefManager.getDownloadOverCellular());
+        binding.highQualityTrack.setChecked(settingsSharedPrefManager.getHighQualityTrack());
+        binding.storeInCache.setChecked(settingsSharedPrefManager.getStoreInCache());
+        binding.explicit.setChecked(settingsSharedPrefManager.getExplicit());
     }
 
     public void backPress(View view) {
         finish();
+    }
+
+    public static final class SettingsSharedPrefManager {
+        SharedPreferences sharedPreferences;
+        public SettingsSharedPrefManager(Context context){
+            sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        }
+
+        public void setDownloadOverCellular(boolean value){
+            sharedPreferences.edit().putBoolean("download_over_cellular", value).apply();
+        }
+        public boolean getDownloadOverCellular(){
+            return sharedPreferences.getBoolean("download_over_cellular", true);
+        }
+
+        public void setHighQualityTrack(boolean value){
+            sharedPreferences.edit().putBoolean("high_quality_track", value).apply();
+        }
+        public boolean getHighQualityTrack(){
+            return sharedPreferences.getBoolean("high_quality_track", true);
+        }
+
+        public void setStoreInCache(boolean value){
+            sharedPreferences.edit().putBoolean("store_in_cache", value).apply();
+        }
+        public boolean getStoreInCache(){
+            return sharedPreferences.getBoolean("store_in_cache", true);
+        }
+
+        public void setExplicit(boolean value){
+            sharedPreferences.edit().putBoolean("explicit", value).apply();
+        }
+        public boolean getExplicit() {
+            return sharedPreferences.getBoolean("explicit", true);
+        }
     }
 }
--- app/src/main/java/com/harsh/shah/saavnmp3/utils/customview/MaterialCustomSwitch.java
+++ app/src/main/java/com/harsh/shah/saavnmp3/utils/customview/MaterialCustomSwitch.java
@@ -11,14 +11,16 @@ import com.harsh.shah.saavnmp3.R;
 
 public class MaterialCustomSwitch extends LinearLayout {
 
-    private String textHead;
-    private String textOn;
-    private String textOff;
-    private boolean checked;
+    private String textHead = "";
+    private String textOn = "";
+    private String textOff = "";
+    private boolean checked = false;
 
     private TextView textHeadView;
     private TextView textDescView;
     private MaterialSwitch materialSwitch;
+
+    private OnCheckChangeListener onCheckChangedListener;
 
     public MaterialCustomSwitch(Context context) {
         super(context);
@@ -40,6 +42,16 @@ public class MaterialCustomSwitch extends LinearLayout {
 
         inflate(getContext(), R.layout.material_custom_switch, this);
 
+        textHeadView = findViewById(R.id.text_head);
+        textDescView = findViewById(R.id.text_desc);
+        materialSwitch = findViewById(R.id.materialSwitch);
+        findViewById(R.id.root).setOnClickListener(v -> materialSwitch.toggle());
+
+        materialSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            textDescView.setText(isChecked ? textOn : textOff);
+            if(onCheckChangedListener!=null)onCheckChangedListener.onCheckChanged(isChecked);
+        });
+
         if(attrs==null) return;
 
         try (TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MaterialCustomSwitch, defStyle, 0)) {
@@ -49,18 +61,23 @@ public class MaterialCustomSwitch extends LinearLayout {
             textOff = a.getString(R.styleable.MaterialCustomSwitch_textOff);
             checked = a.getBoolean(R.styleable.MaterialCustomSwitch_checked, false);
 
-            textHeadView = findViewById(R.id.text_head);
-            textDescView = findViewById(R.id.text_desc);
-            materialSwitch = findViewById(R.id.materialSwitch);
-
             textHeadView.setText(textHead);
             textDescView.setText(checked ? textOn : textOff);
             materialSwitch.setChecked(checked);
-            materialSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> textDescView.setText(isChecked ? textOn : textOff));
 
-            findViewById(R.id.root).setOnClickListener(v -> materialSwitch.toggle());
-
-//            a.recycle();
         }
     }
+
+    public void setOnCheckChangeListener(OnCheckChangeListener onCheckChangedListener) {
+        this.onCheckChangedListener = onCheckChangedListener;
+    }
+
+    public void setChecked(boolean checked){
+        materialSwitch.setChecked(checked);
+    }
+
+    public interface OnCheckChangeListener {
+        void onCheckChanged(boolean isChecked);
+    }
+
 }
