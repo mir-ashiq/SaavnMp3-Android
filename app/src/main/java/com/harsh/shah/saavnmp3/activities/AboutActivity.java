71b127c97564f00d4cc07854517db0037e35597e
Harshshah6 <harshsandeep23@gmail.com>
2024-12-31 13:48:47 +0530

    modified:   app/src/main/java/com/harsh/shah/saavnmp3/activities/AboutActivity.java 	new file:   app/src/main/res/drawable/discord.xml 	new file:   app/src/main/res/drawable/github.xml 	new file:   app/src/main/res/drawable/instagram.xml 	new file:   app/src/main/res/drawable/round_menu_book_24.xml 	new file:   app/src/main/res/drawable/telegram.xml 	modified:   app/src/main/res/layout/activity_about.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/harsh/shah/saavnmp3/activities/AboutActivity.java
+++ app/src/main/java/com/harsh/shah/saavnmp3/activities/AboutActivity.java
@@ -2,25 +2,21 @@ package com.harsh.shah.saavnmp3.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.harsh.shah.saavnmp3.R;
+import com.harsh.shah.saavnmp3.databinding.ActivityAboutBinding;
 
 public class AboutActivity extends AppCompatActivity {
+
+    ActivityAboutBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_about);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationOnClickListener(view -> finish());
     }
 }
