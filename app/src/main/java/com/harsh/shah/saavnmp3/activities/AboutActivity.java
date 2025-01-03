63876f6729bdedadf8cd7aab13f8a5ff7ccd8c7b
Harshshah6 <harshsandeep23@gmail.com>
2025-01-03 15:33:28 +0530

    modified:   app/src/main/java/com/harsh/shah/saavnmp3/activities/AboutActivity.java 	new file:   app/src/main/res/drawable/star_24px.xml 	modified:   app/src/main/res/layout/activity_about.xml

--- app/src/main/java/com/harsh/shah/saavnmp3/activities/AboutActivity.java
+++ app/src/main/java/com/harsh/shah/saavnmp3/activities/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.harsh.shah.saavnmp3.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,5 +21,19 @@ public class AboutActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
         binding.toolbar.setNavigationOnClickListener(view -> finish());
+
+        binding.email.setOnClickListener(view -> openUrl("mailto:harshsandeep23@gmail.com"));
+        binding.sourceCode.setOnClickListener(view -> openUrl("https://github.com/harshshah6/SaavnMp3-Android"));
+        binding.discord.setOnClickListener(view -> Toast.makeText(AboutActivity.this, "Oops, No Discord Server found.", Toast.LENGTH_SHORT).show());
+        binding.instagram.setOnClickListener(view -> openUrl("https://www.instagram.com/harsh_.s._shah/"));
+        binding.telegram.setOnClickListener(view -> openUrl("https://t.me/legendary_streamer_official"));
+
+    }
+
+    private void openUrl(final String url){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_VIEW);
+        sendIntent.setData(Uri.parse(url));
+        startActivity(sendIntent);
     }
 }
